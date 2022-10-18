867841bfe11f1513c377f4e586207ebe2e931d40
jairo <jairoalexis549@gmail.com>
2022-10-18 07:18:26 +0200

    index

---
+++ View/components/slider.php
@@ -0,0 +1,24 @@
+<div id="content-rigth" class="col-xl-4">
+    <div class="container">
+        <div class="content col-xl-9">
+            <div class="card" >
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="content col-xl-9">
+            <div class="card" >
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
---
+++ View/main.php
@@ -0,0 +1,35 @@
+<div id="container-main" class="d-flex flex-direccion-row w-100 h-100 p-5">
+    <div id="content-main" class="content  col-xl-8 d-flex justify-content-center  ">
+        <div id="publicacion" class=" col-xl-10 ">
+            <div class="card border-0 shadow mb-3 w-80 col-xl-12">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body ">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card border-0 shadow mb-3 w-80 col-xl-12">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body ">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card border-0 shadow mb-3 w-80 col-xl-12">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body ">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "components/slider.php" ?>
+
+</div>
