bcd1d6f7db936d34c9d543a8d50906785b40cb84
Ander <ikbsm@plaiaundi.net>
2022-10-20 11:41:14 +0200

    subir cambios

---
+++ Model/Conexion.php
@@ -0,0 +1,25 @@
+<?php
+namespace Model;
+require_once 'Usuario.php';
+
+class Conexion
+{
+    private $con;
+    
+    public function __construct(){
+        $this->con = new \mysqli('10.10.17.136', 'root', '', 'jase');
+    }
+    
+    public function close() {
+        $this->con->close();
+    }
+    
+    public function con(){
+        return $this->con;
+    }
+}
+
+
+
+
+?>
