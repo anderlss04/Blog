04cd141bd3c8a183be4cb4548dbf107f87b9da71
jairo <alexmorocho549@example.com>
2022-10-24 12:36:18 +0200

    nuevo

---
+++ Controller/portadaController.php
@@ -0,0 +1,15 @@
+<?php
+use \Model\M_Posts;
+require ('../Model/M_Posts.php')
+
+
+$posts = new  M_Posts();
+
+
+// require('Model/M_Posts');
+// new Po 
+
+
+// require ('../View/main.php');
+
+?>
--- View/main.php
+++ View/main.php
@@ -11,7 +11,7 @@
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
-           
+              <?php var_dump($posts) ?>
             
         </div>
     </div>
