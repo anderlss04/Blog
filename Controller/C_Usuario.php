b64b12aad4bb0e032ec653615f26438d2f8df180
jairo <alexmorocho549@example.com>
2022-11-03 09:40:23 +0100

    login

--- Controller/C_Usuario.php
+++ Controller/C_Usuario.php
@@ -4,17 +4,17 @@ use Model\M_Usuario;
 require_once('../Model/M_Usuario.php');
 
 $con = new M_Usuario();
-
 $usuario = $con->getUsers();
 
-
-var_dump($usuario);
-
+// var_dump($usuario);
 // $path = '';
+
 $con->close();
+
+
 $con->View('../View/users/user.php', $usuario)
 
-// require_once('../View/users/user.php');
+// require_once('../View/users/mainUuser.php');
 
 
 ?>
