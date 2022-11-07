cbc3ca2eebe908940a3cd83a08bbe3907f0ae9e8
jairo <alexmorocho549@example.com>
2022-11-07 12:35:10 +0100

    cambios

--- View/users/perfilUser.php
+++ View/users/perfilUser.php
@@ -1,8 +1,9 @@
+    <link rel="stylesheet" href="../../src/css/user.css">
+    <?php require '../components/scripts.php'; ?>
 
  <div class="content-img W-100  d-flex justify-content-center align-items-center">
          <div class="content-img-inner d-flex justify-content-center align-items-center">
             <div class="img-perfil">
-               sdiajkxcasd
  
             </div>
             <!-- <img src="../../src/images/perfil.jpg" alt=""> -->
@@ -10,5 +11,25 @@
             <!-- <img src="../../src/images/perfil.jpg" alt="product-1"> -->
     </div> 
 <div class="options d-flex justify-content-center flex-column align-items-center">
-<!-- sok -->
+<div class="opcion">
+  <ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Inicio</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Perfil</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contacto</button>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div>
+
+   
+</div>
+
 </div>
--- View/users/user.php
+++ View/users/user.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil Usuario</title>
+
     <?php require '../components/scripts.php'; ?>
-    <link rel="stylesheet" href="../../src/css/user.css">
 
 </head>
 
