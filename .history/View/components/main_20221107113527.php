5db0f42e700153b62771d8eae6ceaa27b84f9f57
Ander <ikbsm@plaiaundi.net>
2022-11-07 11:43:44 +0100

    subido

--- .history/View/components/main_20221107113527.php
+++ .history/View/components/main_20221107113527.php
@@ -1,9 +1,8 @@
-<?php require "Controller/ver/C_Ver_Todos_Posts.php"; ?>
+
 <div class="d-flex flex-direccion-row w-100 h-100 ">
     <div id="container-main" class="d-flex flex-direccion-row  p-5 ">
         <div id="content-main" class="content  col-xl-12 d-flex justify-content-center overflow-auto  h-50 ">
             <div id="publicacion" class=" col-xl-12  w-75 ">
-
                 <div class="card border-0 shadow mb-3  col-xl-12">
                     <img class="card-img-top" src="..." alt="Card image cap">
                     <div class="card-body ">
