50677061fd230ce7458f324793f029c81251e02c
jairo <alexmorocho549@example.com>
2022-11-08 12:36:31 +0100

    login

--- Controller/Login.php
+++ Controller/Login.php
@@ -1,20 +1,22 @@
-<?php 
+<?php
 use Model\M_Login;
-require_once('../Model/M_Login.php');
+require_once '../Model/M_Login.php';
 
-if(isset($_POST['nombre']) && $_POST['pass'] ){
-    $nombre= $_POST['nombre'];
+if (isset($_POST['nombre']) && $_POST['pass']) {
+    $nombre = $_POST['nombre'];
     $dni = $_POST['pass'];
-    $nuevo = new  M_Login();
-   $datos = $nuevo ->comprobarUser($nombre, $dni);
-   var_dump($datos);
-    if(!$datos){
-        print_r("error usuario o contraseña ");
-        header("Location: ../index.php");
-    }else{
-        session_start();
-        $_SESSION['user'] = $nombre;
+    $nuevo = new M_Login();
+    $datos = $nuevo->comprobarUser($nombre, $dni);
+    // $datos = $nuevo->comprobarUser('tsiggin0', 'AeT9kSDkbn');
+    echo $datos;
 
-        header("Location: ../View/users/perfilUser.php");
-    }
+    // if(!$datos){
+    //     print_r("error usuario o contraseña ");
+    //     header("Location: ../index.php");
+    // }else{
+    //     session_start();
+    //     $_SESSION['user'] = $nombre;
+
+    //     header("Location: ../View/users/perfilUser.php");
+    // }
 }
