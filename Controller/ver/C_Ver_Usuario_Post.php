9954729b3cdd9a0b64b30d75c2822c9b5f6e50cd
jairo <alexmorocho549@example.com>
2022-11-16 09:53:48 +0100

    nuevo

--- Controller/ver/C_Ver_Usuario_Post.php
+++ Controller/ver/C_Ver_Usuario_Post.php
@@ -2,16 +2,18 @@
 namespace Model;
 require_once 'Model/Conexion.php';
 
-class C_Ver_Usuario_Post extends Conexion{
-    public function getPost($idPost){
-        $query = parent::con()->query("SELECT users.username, users.email FROM users INNER JOIN posts ON users.id= posts.userId  WHERE posts.userId = '$idPost'");
-    
-        $fila = $query->fetch_array(); 
+class C_Ver_Usuario_Post extends Conexion
+{
+    public function getPost($idPost)
+    {
+        $query = parent::con()->query(
+            "SELECT users.username, users.email FROM users INNER JOIN posts ON users.id= posts.user_id  WHERE posts.user_id = '$idPost'"
+        );
+
+        $fila = $query->fetch_array();
         return $fila;
     }
     // require_once('../../Index.php');//relleno
-    
-    
 }
 $user = new C_Ver_Usuario_Post();
 ?>
